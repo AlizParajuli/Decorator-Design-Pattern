c293110b4fb952faeb5c5b779f240a28ab4b4662
Aliz <A00290934@mycambrian.ca>
2024-12-11 19:36:35 -0500

    Added decorator abstract class

---
+++ Decorator Design Pattern/Decorator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator_Design_Pattern
+{
+    public abstract class Decorator<T> : IComponent<T>
+    {
+        protected readonly IComponent<T> component;
+
+        public Decorator(IComponent<T> component)
+        {
+            this.component = component;
+        }
+
+        public abstract T GetText();
+    }
+}
